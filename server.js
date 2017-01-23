15c125f2aec57ffa1edc5e053f8c9ca634acfab1
Dan Turco <dan.turco@aescentia.com>
2017-01-23 15:58:36 -0700

    fixed deploy bug for heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!');
 });
 
